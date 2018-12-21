87bd3a703eeefba7c5efb901079fab158725ac33
Olivier Lange <olange@petit-atelier.ch>
2018-12-21 18:11:19 +0100

    Three async `range()` generator functions run in parallel (#4)

--- experiments/02-generators/worker.js
+++ experiments/02-generators/worker.js
@@ -1,11 +1,43 @@
-self.onmessage = (msg) => {
+function sleep( ms) {
+  return new Promise( resolve => setTimeout( resolve, ms));
+}
+
+async function* range( min, max, step = 1, delay = undefined) {
+  console.log( `range(${min}, ${max}, ${step}, ${delay})`);
+  console.assert( typeof delay === "undefined" || typeof delay === "number",
+    `delay should be either undefined or a number (${delay})`);
+  console.assert( typeof min === "number" && typeof max === "number" && typeof step === "number",
+    `min, max and step should numbers (${min}, ${max} and ${step})`);
+  console.assert( step != 0, `To guard against infinite range,
+    step should be strictly positive or negative (${step})`);
+  if( step > 0) console.assert( min <= max,
+    `When step (${step}) is positive, min (${min}) <= max (${max})`);
+  if( step < 0) console.assert( min >= max,
+    `When step (${step}) is negative, min (${min}) >= max (${max})`);
+
+  let i = min;
+  for(; step >= 0 ? i <= max : i >= max; i += step) {
+    yield i;
+    if( delay)
+      await sleep( delay);
+  }
+}
+
+self.onmessage = async (msg) => {
   const data = msg.data;
   console.log( "worker.js › Received message", data);
-  switch( typeof data.opcode) {
-    case "undefined":
-      throw new Error( `Zapit! Received unknown command ${data.opcode}`);
-    default:
-      self.postMessage( { ack: data });
+  switch( data.opcode) {
+    case "sip":
+    case "crunch":
+      self.postMessage( { [data.opcode]: data.args });
       break;
+    case "range":
+      const name = `range(${data.args})`;
+      for await (let val of range( ...data.args)) {
+        self.postMessage( { [name]: val });
+      }
+      break;
+    default:
+      throw new Error( `Zapit! Received unknown command ${data.opcode}`);
   }
 };
--- experiments/shared-func.js
+++ experiments/shared-func.js
@@ -4,8 +4,11 @@ export function appendMsgTo( outputElem, childElemName, msg) {
   const fragment = document.createDocumentFragment();
   const newChildElem = document.createElement( childElemName);
 
-  newChildElem.appendChild(
-    document.createTextNode( String( msg)));
+  // Poor man's templating (combined with a template literal to
+  // inject values, innerHTML gives us a way to generate DOM content)
+  newChildElem.innerHTML = String( msg);
+  // To avoid HTML injection, you would rather use:
+  // newChildElem.appendChild( document.createTextNode( String( msg)));
 
   fragment.appendChild( newChildElem);
   outputElem.appendChild( fragment);
