9090fe24d2b714dddd66ac3531e4090d238f35f7
Olivier Lange <olange@petit-atelier.ch>
2018-12-22 20:19:27 +0100

    Updates protocol of messages passed in/out workers in experiment template

--- experiments/template/worker.js
+++ experiments/template/worker.js
@@ -1,11 +1,13 @@
 self.onmessage = (msg) => {
   const data = msg.data;
   console.log( "worker.js › Received message", data);
-  switch( typeof data.opcode) {
-    case "undefined":
-      throw new Error( `Zapit! Received unknown command ${data.opcode}`);
-    default:
-      self.postMessage( { ack: data });
+  switch( data.op) {
+    case "sip":
+    case "crunch":
+    case "listen":
+      self.postMessage( { ...data, result: "ack" });
       break;
+    default:
+      throw new Error( `Zapit! Received unknown command ${data.op}`);
   }
 };
